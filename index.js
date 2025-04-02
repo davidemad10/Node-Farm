67d992cb6adcb64c3b268db227898fb52edcc488
David Emad <demad2527@gmail.com>
2025-04-02 15:41:30 +0200

    add modules

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { log } = require("console");
 const fs = require("fs");
-
+const replacetemp = require("./modules/replacetemp");
 /////////////////////////////////////////
 // FILES
 /*
@@ -57,20 +57,6 @@ const tempProduct = fs.readFileSync(
 );
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
-
-const replacetemp = (temp, product) => {
-  let output = temp.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRODUCT_NAME%}/g, product.productName);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  return output;
-};
 
 const server = http.createServer((req, res) => {
   // const pathName = req.url;
